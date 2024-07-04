c7196786aa1629c9be65d3c44cc74b4f27c167cf
Emre Ozturk <emreeozturk42@gmail.com>
2024-07-04 05:07:15 +0300

    chore: Add PostPage component to display all posts

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,12 @@
+
+const PostPage = () => {
+    return (
+        <div>
+            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
+                All Posts
+            </h1>
+        </div>
+    )
+}
+
+export default PostPage
